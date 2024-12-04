302991b6b4b0d0c2622f259a5bab7c8699b6135b
Christian Nuss <christian.nuss@gmail.com>
2024-12-04 06:03:53 -0500

    WIP

--- app.py
+++ app.py
@@ -1,10 +1,1 @@
-from flask import Flask
-from transformers import pipeline
-
-app = Flask(__name__)
-generator = pipeline('text-generation', model='openai-community/gpt2')
-
-@app.route("/")
-def hello_world():
-    output = generator("Hello, world! I")
-    return output[0]['generated_text']
+print("hello world")
